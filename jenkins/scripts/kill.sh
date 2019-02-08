0879afb89fd1fbeb976ffafb4b545c2e6a1d1624
yiezi <11764404@qq.com>
2019-02-08 16:17:05 +0800

    fix: 脚本无法执行bug

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,8 +4,7 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-kill -9 $(ps -ef | grep $(cat .pidfile))
-if [(ps -o pid |grep $(cat .pidfile))!=""];then
+if [ps -o pid |grep $(cat .pidfile)!=""];then
     echo "you have old progress need to kill"
     kill $(cat .pidfile)
 fi
